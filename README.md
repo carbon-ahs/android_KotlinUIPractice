73d7a7fa6c15c0dbbb67a10ede877ac1340bcab3
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-05-11 16:43:22 +0600

    counter app done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # android_KotlinUIPractice
+
+https://www.youtube.com/watch?v=evdsqMgB5sE&list=PLfuE3hOAeWhYSuEz7CkfD2WFH97Obt9fK&index=11
--- app/src/main/java/com/axiagroups/kotlinuipractice/MainActivity.kt
+++ app/src/main/java/com/axiagroups/kotlinuipractice/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.axiagroups.kotlinuipractice
 
 import android.os.Bundle
-import android.provider.CalendarContract.Colors
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,16 +11,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.axiagroups.kotlinuipractice.ui.theme.KotlinUIPracticeTheme
@@ -48,12 +45,12 @@ fun MyApp(content: @Composable () -> Unit) {
 
 @Composable
 fun MainContent() {
-    var counter = 0
+    var counter by rememberSaveable { mutableStateOf(0) }
     var incrementCounter = {
-        counter++
+        counter += 1
     }
     var decrementCounter = {
-        counter--
+        counter -= 1
     }
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -72,7 +69,7 @@ fun MainContent() {
             Button(onClick = { incrementCounter.invoke() }, modifier = Modifier.padding(8.dp)) {
                 Text(text = "+")
             }
-            Text(text = "$counter")
+            Text(text = "${counter}")
             Button(onClick = { decrementCounter.invoke() }, modifier = Modifier.padding(8.dp)) {
                 Text(text = "-")
             }
